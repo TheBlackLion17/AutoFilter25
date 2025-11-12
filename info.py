b07924a75e989803568fab451fdf93b2b903802c
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-11-12 23:10:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ def is_enabled(value, default):
 # ============================
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '13357171'))
-API_HASH = environ.get('API_HASH', 'd39c4324a40a8a6b27a067f8ff2b987e'')
+API_HASH = environ.get('API_HASH', 'd39c4324a40a8a6b27a067f8ff2b987e')
 BOT_TOKEN = environ.get('BOT_TOKEN', "8161552461:AAEIRDGMbwBDIrtjmVeD9k6DxdIf55KuN3s")
 
 # ============================
