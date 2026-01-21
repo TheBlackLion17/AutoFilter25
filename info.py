f877517fe26288198814e9de211d6a74f4416995
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2026-01-21 17:26:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -99,7 +99,7 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Movies_Hub_OG')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/AgsModsOG')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/AgsModsOG')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/AgsModsOG')
 MOVIE_UPDATE_CHANNEL_LNK = environ.get('MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/Ags_Updates')
